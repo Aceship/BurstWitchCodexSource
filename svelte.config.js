427b002f05b35ba20ada7d9fc9269576197d8d69
Aceship <terrenceleroy@yahoo.com>
2021-11-03 00:20:51 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
-import node from '@sveltejs/adapter-static';
-
+import adapter from '@sveltejs/adapter-static';
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: node({out:'./out'}),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		paths: {
 			base: dev ? '' : '/BurstWitchCodex',
 		},
+		appDir: 'internal',
 	}
 };
 
